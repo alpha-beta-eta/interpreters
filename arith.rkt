54d36e8948d456ea2a556f4b7f664b981bb997c7
alpha-beta-eta <95096031+alpha-beta-eta@users.noreply.github.com>
2022-03-09 08:27:53 +0800

    Update arith.rkt

--- arith.rkt
+++ arith.rkt
@@ -8,8 +8,8 @@
     (,int (guard (integer? int)) int)
     ((,op ,e1 ,e2)
      (guard (memq op '(+ - *)))
-     (let ((v1 (interp e1))
-           (v2 (interp e2)))
+     (let* ((v1 (interp e1))
+            (v2 (interp e2)))
        (case op
          ((+) (+ v1 v2))
          ((-) (- v1 v2))
